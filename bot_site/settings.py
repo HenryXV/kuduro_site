bcb782eccf4639a7f9a891c4584a8146c387a2a1
Henrique <henri.r.s@hotmail.com>
2020-10-16 23:22:12 -0300

    settings

--- bot_site/settings.py
+++ bot_site/settings.py
@@ -74,14 +74,6 @@ CSRF_COOKIE_SECURE = True
 
 SESSION_COOKIE_SECURE = True
 
-SECURE_SSL_REDIRECT = True
-
-SECURE_HSTS_SECONDS = 31536000
-
-SECURE_HSTS_PRELOAD = True
-
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
