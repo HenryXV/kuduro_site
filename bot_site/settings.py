7610bf1e55074db60b640a70c531344a35243afb
Henrique <henri.r.s@hotmail.com>
2020-10-17 00:19:18 -0300

    settings

--- bot_site/settings.py
+++ bot_site/settings.py
@@ -165,6 +165,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals(), staticfiles=False, logging=False)
