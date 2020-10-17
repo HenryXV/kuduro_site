d4836e43465340b3adb483b6b987a2a61b4fa41f
Henrique <henri.r.s@hotmail.com>
2020-10-16 23:23:11 -0300

    url

--- bot_site/urls.py
+++ bot_site/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('docs/', include('docs.urls')),
+    path('', include('docs.urls')),
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
 ]
