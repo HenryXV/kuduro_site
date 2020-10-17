1592b0890b1581e9a8a40603ca14c661520d172b
Henrique <henri.r.s@hotmail.com>
2020-10-16 22:24:58 -0300

    setting change

--- bot_site/bot_site/settings.py
+++ bot_site/bot_site/settings.py
@@ -9,10 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import os
-
 from pathlib import Path
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-load_dotenv()
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "SECRET_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -90,14 +86,6 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd4du7ka5gr234l',
-        'USER': 'okxjldpusnptnb',
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': 'ec2-3-218-75-21.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
 }
 
 
