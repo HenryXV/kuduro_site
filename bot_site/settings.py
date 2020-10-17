9be77c7dccd9cf22f096c82377deca2436de1a85
Henrique <henri.r.s@hotmail.com>
2020-10-16 23:27:52 -0300

    settings

--- bot_site/settings.py
+++ bot_site/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = "SECRET_KEY"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://kudurosite.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
